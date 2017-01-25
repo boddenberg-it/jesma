2d626cc7e36358b6fd45eb2877dc6bc07b9cf54a
André Boddenberg <andre@boddenberg.it>
2017-01-25 23:27:59 +0100

    adding isManuallyConnected and other functionalities, futhermore introducing getJson method to avoid code duplication

--- libs/JenkinsSlave.groovy
+++ libs/JenkinsSlave.groovy
@@ -3,41 +3,86 @@ import groovy.json.JsonSlurper
 
 class JenkinsSlave {
 
-  String jnlpUrl
-  String serial
-  String slaveApiUrl
-  String slaveName
+  private final String jenkinsHost
+  private final String jnlpUrl
+  private final String slaveName
+  private final String secret
+  private final String serial
 
-  UNIXProcess process
+  private UNIXProcess process
 
-  // extract method! into own class Jenkins slave
-  /***** Jenkins slave methods****/
   boolean startSlave() {
     // sanity check whether log dir is accessible
     //assertTrue new File(./slaveLogs).isAccessible()
+
     Date date = new Date().format('yyyy-MM-dd_hh:mm:ss')
 
     this.process = """
-                     java -jar slave.jar
-                        -slaveLog ./slaveLogs/${date}_${slaveName}.${serial}.log
-                        -jnlpUrl ${jnlpUrl}
-                        -secret
-                   """.stripIndent().execute()
+      java -jar slave.jar
+         -slaveLog ./logs/slaves/${date}_${this.slaveName}.${this.serial}.log
+         -jnlpUrl ${this.jnlpUrl}
+         -secret ${this.secret}
+    """.stripIndent().execute()
 
-    sleep(5000)
+    sleep(3000)
     this.isOnline()
   }
 
   boolean killSlave() {
     this.process.destroy()
-    sleep(5000)
+    sleep(3000)
     !this.isOnline()
   }
 
+  // is'es methods
   boolean isOnline() {
-    String url = "${jenkinsHost}/computer/${this.slavName}/api/json?tree=offline"
+    // this.getJson("offline").offline one line, if getJson works :D
+    String url = "${this.jenkinsHost}/computer/${this.slavName}/api/json?tree=offline"
     def jsonResp = new JsonSlurper().parseText(url.toURL().getText())
     !jsonResp.offline
+  }
+
+
+  // returns null (as String) or the reason why user disconnected slave
+  String isManuallyDisconnected() {
+    String url = "${this.jenkinsHost}/computer/${this.slavName}/api/json?tree=offlineCause[description]"
+    def jsonResp = new JsonSlurper().parseText(url.toURL().getText())
+    if(!jsonResp.offlineCause) { return null }
+    jsonResp.offlineCause.description
+  }
+
+  boolean isBuilding() {
+    // this.getJson("executors[idle]").idle one line, if getJson works :D
+    String url = "${this.jenkinsHost}/computer/${this.slavName}/api/json?tree=executors[idle]"
+    def jsonResp = new JsonSlurper().parseText(url.toURL().getText())
+    jsonResp.idle
+  }
+
+  // have to try it, but will make the two methods smaller!
+  def getJson(String query) {
+    String url = "${this.jenkinsHost}/computer/${this.slavName}/api/json?tree=${query}"
+    new JsonSlurper().parseText(url.toURL().getText())
+  }
+
+  // setter methods
+  void setJenkinsHost(String host) {
+    this.jenkinsHost = host
+  }
+
+  void setJnlpUrl(String jnlpUrl) {
+    this.jnlpUrl = jnlpUrl
+  }
+
+  void setSlaveName(String serial) {
+    this.serial = serial
+  }
+
+  void setSecret(String secret) {
+    this.secret = secret
+  }
+
+  void setSerial(String serial) {
+    this.serial = serial
   }
 
 }
