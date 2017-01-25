2c1347d9b17def9e61e3277450536bbb070a622b
blobb <blobb@blobbnas.fritz.box>
2017-01-25 04:05:10 +0100

    stopping now, extract Jenkins slave logic into own class

--- libs/AndroidDevice.groovy
+++ libs/AndroidDevice.groovy
@@ -1,8 +1,35 @@
 #!/usr/bin/groovy
-class AndroidDevice {
-  String state
-  String serial
 
+class AndroidDevice {
+  // RaspberryPi only
+  int powerGpio
+
+  String serial
+  String state
+
+  String slaveName
+  String jnlpUrl
+  UNIXProcess process
+
+
+  // extract method! into own class Jenkins slave
+  /***** Jenkins slave methods****/
+  UNIXProcess startSlave() {
+    // sanity check whether log dir is accessible
+    assertTrue new File(./slaveLogs).isAccessible()
+
+    return """java -jar slave.jar
+                   -slaveLog ./slaveLogs/date.${slaveName}.${serial}.log
+                   -jnlpUrl ${jnlpUrl}
+                   -secret""".execute()
+    // process.destroy() <- to kill!
+  }
+
+  void killSlave() {
+    this.process.destroy()
+  }
+
+  /***** Android  ******/
   int getBatteryCapacity() {
     return Integer.parseInt("adb -s ${this.serial} shell cat /sys/class/power_supply/battery/capacity"
       .execute().text.trim())
@@ -11,5 +38,17 @@ class AndroidDevice {
   String isCharging(){
     return "adb -s ${this.serial} shell cat /sys/class/power_supply/battery/status"
       .execute().text.trim()
+  }
+
+  boolean pingGoogle() {
+
+  }
+
+  boolean isAirplaneModeOn() {
+
+  }
+
+  boolean setAirplaneMode() {
+
   }
 }
