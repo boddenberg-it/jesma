cde0f06e5b28077f965541de67b854dd853e9f87
blobb <blobb@blobbnas.fritz.box>
2017-01-25 02:33:28 +0100

    Adding file for introduced AndroidDevice class and first status functionality

--- libs/AndroidDevice.groovy
+++ libs/AndroidDevice.groovy
@@ -2,4 +2,14 @@
 class AndroidDevice {
   String state
   String serial
+
+  int getBatteryCapacity() {
+    return Integer.parseInt("adb -s ${this.serial} shell cat /sys/class/power_supply/battery/capacity"
+      .execute().text.trim())
+  }
+
+  String isCharging(){
+    return "adb -s ${this.serial} shell cat /sys/class/power_supply/battery/status"
+      .execute().text.trim()
+  }
 }
